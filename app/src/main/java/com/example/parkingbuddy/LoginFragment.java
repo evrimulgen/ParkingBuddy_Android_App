9b19925012351540cf2daf19dc975be32ab523fe
Madhav Chhura <chhura.madhav@gmail.com>
2015-05-10 18:44:12 -0700

    Fixed Error in  LoginFragment class

--- app/src/main/java/com/example/parkingbuddy/LoginFragment.java
+++ app/src/main/java/com/example/parkingbuddy/LoginFragment.java
@@ -150,7 +150,7 @@ public class LoginFragment extends LoginFragmentBase {
                                 loadingFinish();
                                 if (e != null) {
                                     //debugLog(getString(R.string.login_warning_parse_login_failed) + e.toString());
-                                    if (e.getMessage() == Exception.OBJECT_NOT_FOUND) {
+                                    if (e.getCode() == ParseException.OBJECT_NOT_FOUND) {
                                         if (config.getLoginInvalidCredentialsToastText() != null) {
                                             showToast(config.getLoginInvalidCredentialsToastText());
                                         } else {
